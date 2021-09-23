451457215fa0b8c4dc50c748bf2bbb341010b646
lastsunset <alexnaznvkz@gmail.com>
2021-09-23 18:03:37 +0400

    1
    1

---
+++ client/defines.h
@@ -0,0 +1,10 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define APPLICATION_NAME    "AmneziaVPN"
+#define SERVICE_NAME        "AmneziaVPN-service"
+#define ORGANIZATION_NAME   "AmneziaVPN.ORG"
+#define APP_MAJOR_VERSION   "1.8.1"
+#define APP_VERSION         "1.8.1.0"
+
+#endif // DEFINES_H
